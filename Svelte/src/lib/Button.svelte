aea6676798590b95cfbf673d2a4d33def6f5e9dd
KanekoKKK <231205066@ccmailg.meijo-u.ac.jp>
2025-11-04 14:38:54 +0900

    Button.sveltのテキストcss変更

--- Svelte/src/lib/Button.svelte
+++ Svelte/src/lib/Button.svelte
@@ -8,19 +8,24 @@
 
 <style>
   button {
-        flex: 1;
-        padding: 20px;
-        text-decoration: none; /* リンクの下線を消す */
-        color: inherit; /* 文字の色を親要素から継承する */
-        border: 1px solid #ddd;
-        border-radius: 8px;
-        transition: all 0.2s ease;
-        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* 要素の下に影を付ける */
-        text-align: center;
+      flex: 1;
+      padding: 20px;
+      text-decoration: none; 
+      border: 1px solid #ddd;
+      border-radius: 8px;
+      transition: all 0.2s ease;
+      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+      
+      text-align: center;
+      font-weight: bold; 
+      font-size: 1.5rem;
+      background-color: #ffffff;
+      color: #00796b;       
   }
+
   button:hover {
-            transform: translateY(-5px);
-        box-shadow: 0 8px 15px rgba(0, 0, 0, 0.15);
-        background-color: #e8f5e9;
+      transform: translateY(-5px);
+      box-shadow: 0 8px 15px rgba(0, 0, 0, 0.15);
+      background-color: #e8f5e9;
   }
 </style>
