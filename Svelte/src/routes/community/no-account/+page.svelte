e9a22e0e38ff120e5fe37414fea7627026d2f63e
Haruya Kouduma <231205082@ccmailg.meijo-u.ac.jp>
2025-12-09 13:42:44 +0900

    no-account内のメッセージを追加

---
+++ Svelte/src/routes/community/no-account/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Title } from '$lib';
+	import FeatureCard from '$lib/features/government/components/FeatureCard.svelte';
+	import alertIcon from '$lib/assets/alert.png';
+	import supportIcon from '$lib/assets/support.png';
+	import mapIcon from '$lib/assets/map.png';
+</script>
+
+<Title
+    iconSrc={alertIcon}
+	iconAlt="警告アイコン"
+	titleText="コミュニティを検知できませんでした。"
+	subtitleText="新規コミュニティ作成もしくはログイン処理を行ってください。"
+/>
